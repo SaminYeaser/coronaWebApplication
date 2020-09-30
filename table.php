bf7d90ca82b7385a6697192438dd6ec2916867d3
Samin <saminyeaser1@gmail.com>
2020-10-01 01:10:41 +0600

    done!!!

--- table.php
+++ table.php
@@ -8,53 +8,61 @@ $result = mysqli_query($conn, $sql);
 $rows = [];
 
 
-
 ?>
-<table class="table table-hover">
-    <thead>
-    <tr align="center">
-        <th scope="col">FirstName</th>
-        <th scope="col">LastName</th>
-        <th scope="col">Gender</th>
-        <th scope="col">Temperature</th>
-        <th scope="col">Age</th>
-        <th scope="col">Date of Checkup</th>
-        <th scope="col">Result</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php
+<div class="table">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <table class="table table-hover">
+                    <thead>
+                    <tr align="center">
+                        <th scope="col">FirstName</th>
+                        <th scope="col">LastName</th>
+                        <th scope="col">Gender</th>
+                        <th scope="col">Temperature</th>
+                        <th scope="col">Age</th>
+                        <th scope="col">Date of Checkup</th>
+                        <th scope="col">Result</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
 
-    if (mysqli_num_rows($result) > 0) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            $rows[] = $row;
-            $firstName = $row['firstname'];
-            $LastName = $row['lastname'];
-            $age = $row['age'];
-            $temparature = $row['temp'];
-            $assesdate = $row['assesDate'];
-            $gender = $row['sex'];
-            $resultCovid = $row['result'];
-            ?>
-            <tr align="center">
+                    if (mysqli_num_rows($result) > 0) {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            $rows[] = $row;
+                            $firstName = $row['firstname'];
+                            $LastName = $row['lastname'];
+                            $age = $row['age'];
+                            $temparature = $row['temp'];
+                            $assesdate = $row['assesDate'];
+                            $gender = $row['sex'];
+                            $resultCovid = $row['result'];
+                            ?>
+                            <tr align="center">
 
-                <td><?php echo $row['firstname']?></td>
-                <td><?php echo $row['lastname'] ?></td>
-                <td><?php echo $row['sex'] ?></td>
-                <td><?php echo $row['temp'] ?></td>
-                <td><?php echo $row['assesDate'] ?></td>
-                <td><?php echo $row['age'] ?></td>
-                <td><?php echo $row['result'] ?></td>
-            </tr>
-            <?php
-        }
-    } else {
-        echo 'Something went wrong';
-    }
-
-
-    ?>
+                                <td><?php echo $row['firstname'] ?></td>
+                                <td><?php echo $row['lastname'] ?></td>
+                                <td><?php echo $row['sex'] ?></td>
+                                <td><?php echo $row['temp'] ?></td>
+                                <td><?php echo $row['assesDate'] ?></td>
+                                <td><?php echo $row['age'] ?></td>
+                                <td><?php echo $row['result'] ?></td>
+                            </tr>
+                            <?php
+                        }
+                    } else {
+                        echo 'Something went wrong';
+                    }
 
 
-    </tbody>
-</table>
+                    ?>
+
+
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+    </div>
+</div>
